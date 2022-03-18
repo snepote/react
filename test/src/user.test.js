d71a2149cdd86f607763e2d61c102372cb70eff7
Sebastian Nepote <sebastian.nepote@helpling.com>
2022-03-18 15:46:19 +0100

    testing: data fetching

---
+++ test/src/user.test.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+import User from "./user";
+
+let container = null;
+beforeEach(() => {
+  // setup DOM element as render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it("renders user data", async () => {
+  const fakeUser = {
+    name: "John Doe",
+    age: "26",
+    address: "123, Fantastic avenue"
+  };
+  jest.spyOn(global, "fetch").mockImplementation(() =>
+    Promise.resolve({
+      json: () => Promise.resolve(fakeUser)
+    })
+  );
+
+  // Use the asynchronous version of act to apply resolved promises
+  await act(async () => {
+    render(<User id="123" />, container);
+  });
+
+  expect(container.querySelector("summary").textContent).toBe(fakeUser.name);
+  expect(container.querySelector("strong").textContent).toBe(fakeUser.age);
+  expect(container.textContent).toContain(fakeUser.address);
+
+  // remove the mock to make sure tests are completely isolated
+  global.fetch.mockRestore();
+});
